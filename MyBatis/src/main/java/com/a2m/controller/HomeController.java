0e3dfa1a265e3b0852e4913996167a4f1c1bb4fc
tavannam95 <89097530+tavannam95@users.noreply.github.com>
2023-03-27 22:24:46 +0700

    main

--- MyBatis/src/main/java/com/a2m/controller/HomeController.java
+++ MyBatis/src/main/java/com/a2m/controller/HomeController.java
@@ -2,10 +2,8 @@ package com.a2m.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.a2m.entities.User;
 
 @Controller
 public class HomeController {
